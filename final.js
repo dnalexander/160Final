dd42ed21bef736a3f77e089c55a0eb5c7dd5926b
Donovan <dnab0012@gmail.com>
2015-06-05 21:34:47 -0700

    loading as parts

--- final.js
+++ final.js
@@ -2,9 +2,6 @@
  * Created by Donovan on 6/1/2015.
  */
 var lamp = new THREE.Group();
-var data = {
-    name: ""
-}
 var scene = new THREE.Scene();
 scene.add(lamp);
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -29,7 +26,9 @@ scene.add( light );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
+document.addEventListener("mousedown", inputDown);
+document.addEventListener("mouseup", inputUp);
+document.addEventListener("mousemove", inputMove);
 /*var jsonLoader = new THREE.JSONLoader();
 jsonLoader.load("lamp_joined.js", createScene);
 function createScene(geometry, materials){
@@ -40,72 +39,51 @@ function createScene(geometry, materials){
     console.log(mesh);
     lamp.add(mesh);
 }*/
-data.name = "lamp_base";
-loadPart(data.name);
-data.name = "top_neck_sides";
-loadPart(data.name);
-data.name = "top_neck_pin_top"
-loadPart(data.name);
-data.name = "top_neck_pin_bottom"
-loadPart(data.name);
-data.name = "top_neck_pin_back"
-loadPart(data.name);
-data.name = "right_spring"
-loadPart(data.name);
-data.name = "right_bands"
-loadPart(data.name);
-data.name = "neck_pin_back"
-loadPart(data.name);
-data.name = "left_spring"
-loadPart(data.name);
-data.name = "left_bands"
-loadPart(data.name);
-data.name = "head_to_neck"
-loadPart(data.name);
-data.name = "head_to_bulb"
-loadPart(data.name);
-data.name = "head_case"
-loadPart(data.name);
-data.name = "front_spring_pin"
-loadPart(data.name);
-data.name = "center_pin_mid"
-loadPart(data.name);
-data.name = "center_pin_front"
-loadPart(data.name);
-data.name = "center_pin_back"
-loadPart(data.name);
-data.name = "center_neck_front"
-loadPart(data.name);
-data.name = "center_neck_back"
-loadPart(data.name);
-data.name = "bulb"
-loadPart(data.name);
-data.name = "bottom_pin_mid"
-loadPart(data.name);
-data.name = "bottom_pin_front"
-loadPart(data.name);
-data.name = "bottom_pin_back"
-loadPart(data.name);
-data.name = "bottom_neck_to_base"
-loadPart(data.name);
-data.name = "bottom_neck_sides"
-loadPart(data.name);
-data.name = "bottom_brace_sides"
-loadPart(data.name);
-data.name = "base_to_neck_sides"
-loadPart(data.name);
-data.name = "base_to_neck_tower"
-loadPart(data.name);
+loadPart("lamp_base");
+loadPart("top_neck_sides");
+loadPart("top_neck_pin_top");
+loadPart("top_neck_pin_bottom");
+loadPart("top_neck_pin_back");
+loadPart("right_spring");
+loadPart("right_bands");
+loadPart("neck_pin_back");
+loadPart("left_spring");
+loadPart("left_bands");
+loadPart("head_to_neck");
+loadPart("head_to_bulb");
+loadPart("head_case");
+loadPart("front_spring_pin");
+loadPart("center_pin_mid");
+loadPart("center_pin_front");
+loadPart("center_pin_back");
+loadPart("center_neck_front");
+loadPart("center_neck_back");
+loadPart("bulb");
+loadPart("bottom_pin_mid");
+loadPart("bottom_pin_front");
+loadPart("bottom_pin_back");
+loadPart("bottom_neck_to_base");
+loadPart("bottom_neck_sides");
+loadPart("bottom_brace_sides");
+loadPart("base_to_neck_sides");
+loadPart("base_to_neck_tower");
 function loadPart(name){
     var jsonLoader = new THREE.JSONLoader();
-    jsonLoader.load("./modelData/"+name+".js", addData);
+    jsonLoader.load("./modelData/"+name+".js", function(geometry, materials){
+        var mat = new THREE.MeshPhongMaterial({
+            shading: THREE.SmoothShading
+        });
+        var mesh = new THREE.Mesh(geometry, mat);
+        mesh.name = name;
+        lamp.add(mesh);
+    });
 }
 function addData(geometry, materials){
     var mat = new THREE.MeshPhongMaterial({
         shading: THREE.SmoothShading
     });
     var mesh = new THREE.Mesh(geometry, mat);
-    mesh.name = data.name;
+    mesh.name = name;
     lamp.add(mesh);
 }
 console.log(lamp);
--- handleInput.js
+++ handleInput.js
@@ -1,27 +1,76 @@
 /*
- * Donovan Alexander (donalexa) 5/11/15 : Program 3 - Transformable Sharks
+ * Donovan Alexander (donalexa) 5/27/15 : Program 4 - Camera Functionality
  This program renders 3D Models of sharks and allows the user to manipulate them by translating, rotating,
- and scaling them.
+ and scaling them. It also allows the user to move the camera as they choose.
  */
-function handleInput(e){
-    //This function handles mouse clicks on the canvas to determine where you clicked and if a shark was selected
-	var rect = canvas.getBoundingClientRect();
-    //Set color picking vars
-	var bgColor = 4294934656;
-    var sharkOneColor = 4294902015;
-    var sharkTwoColor = 4294901760;
-	mouseXPos = (e.clientX-rect.left);
-	mouseYPos = (-(e.clientY-rect.top-canvas.width));
-	//left click
-	if(e.button == 0){
-        //on left click, set the active shark according to which one was clicked
-		var pixelColor = colorData[(canvas.width*mouseYPos)+mouseXPos];
-		if(pixelColor != bgColor && pixelColor == sharkOneColor){
-            activeShark = "pink";
-		} else if(pixelColor != bgColor && pixelColor == sharkTwoColor){
-            activeShark = "blue";
-        } else{
-            activeShark = "none";
+//This holds the variables for the starting position of a click
+var source = {
+    mouseXPos:-1,
+    mouseYPos:-1
+};
+var pressed;
+var windowHalfX = window.innerWidth/2;
+var windowHalfY = window.innerHeight/2;
+function inputDown(e){
+    //This function handles mouse clicks on the canvas to determine where you clicked and if a shark was selected\
+    source.mouseXPos = (e.clientX-windowHalfX) * 10;
+    source.mouseYPos = (-(e.clientY-windowHalfX)) * 10;
+    if(e.type == "mousedown"){
+        pressed = true;
+        /*//Set color picking vars
+        var bgColor = 4294934656;
+        var sharkOneColor = 4294902015;
+        var sharkTwoColor = 4294901760;
+        //left click
+        if(e.button == 0){
+            //on left click, set the active shark according to which one was clicked
+            var pixelColor = colorData[(canvas.width*source.mouseYPos)+source.mouseXPos];
+            if(pixelColor != bgColor && pixelColor == sharkOneColor){
+                activeShark = "Pink";
+            } else if(pixelColor != bgColor && pixelColor == sharkTwoColor){
+                activeShark = "Blue";
+            } else{
+                activeShark = "None";
+            }
+            document.getElementById("shark").innerHTML = activeShark;*/
+    }
+}
+
+function inputUp(e){
+    //if you release the mouse button, let the program know
+    pressed = false;
+}
+
+function inputMove(e){
+    //This function handles the dragging of the moues
+    if(e.button == 0 && pressed) {
+        //This is the case where you have left clicked and are holding the button
+        //We calculate the new (x, y) of the mouse
+        var newX = (e.clientX - windowHalfX) * 10;
+        var newY = (-(e.clientY - windowHalfY)) * 10;
+        //change the camera position based on where you drag the mouse
+        //X-axis movement is determined by moving the mouse left/right of its originally clicked point
+        //Y-axis movement is determined by moving the mouse above/below its originally clicked point
+        if (newX > source.mouseXPos) {
+            camera.position.x += 0.01;
+        } else if (newX < source.mouseXPos) {
+            camera.position.x -= 0.01;
         }
-	}
+        if (newY > source.mouseYPos) {
+            camera.position.y += 0.01;
+        } else if (newY < source.mouseYPos) {
+            camera.position.y -= 0.01;
+        }
+    }
+    if(e.button == 2 && pressed){
+        //This is the case where you have right clicked and are holding the button
+        //We only need a new y position because we move up and down to zoom
+        var newY = (-(e.clientY-windowHalfY)) * 10;
+        //If we move up, move the camera "forward", if we move down, move the camera "backwards"
+        if(newY > source.mouseYPos){
+            camera.position.z += 0.01;
+        } else if(newY < source.mouseYPos){
+            camera.position.z -= 0.01;
+        }
+    }
 }
